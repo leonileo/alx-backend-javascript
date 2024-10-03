2e672efad016e20875bc462da5ca9cdb0abc0ceb
leonileo <leolittleprogrammer@gmail.com>
2024-10-03 11:34:43 +0000

    Block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  task = false;
-  task2 = true;
-
-  if (trueOrFalse) {
-    var task;
-    var task2;
-  }
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
