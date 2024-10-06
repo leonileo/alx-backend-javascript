e3768f6531f4085bc87d6441457d5167ba787f47
leonileo <leolittleprogrammer@gmail.com>
2024-10-06 06:20:52 +0000

    Rest parameter

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,9 @@
 export default function returnHowManyArguments(...rest) {
-    let total = 0;
-    for (const arg of rest) {
-	total++;
+  let total = 0;
+  for (const arg of rest) {
+    if (arg) {
+      total += 1;
     }
-    return total;
+  }
+  return total;
 }
