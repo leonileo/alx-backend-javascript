862c02a892270a153caef812b327d97cec326a13
leonileo <leolittleprogrammer@gmail.com>
2024-10-08 17:03:19 +0000

    Hot fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((all) => {
-    console.log(`${all[0].body} ${all[1].firstName} ${all[1].lastName}`);
+    console.log(all[0].body, all[1].firstName, all[1].lastName);
   }).catch(() => console.log('Signup system offline'));
 }
