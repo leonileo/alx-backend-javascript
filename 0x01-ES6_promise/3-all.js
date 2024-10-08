019404d6d24fa64336737ea29fa0eb0568216962
leonileo <leolittleprogrammer@gmail.com>
2024-10-08 14:16:27 +0000

    All

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((all) => {
     console.log(`${all[0].body} ${all[1].firstName} ${all[1].lastName}`);
   }).catch(() => console.log('Signup system offline'));
