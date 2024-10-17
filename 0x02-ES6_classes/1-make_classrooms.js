46f3be25c542d9db0e6cd1e3c5752ffe2fdec01c
leonileo <leolittleprogrammer@gmail.com>
2024-10-17 14:46:55 +0000

    hot fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,13 @@
 import ClassRoom from './0-classroom';
 
-/**
- * Creates an array of {@link ClassRoom}s with a specific size.
- * @returns An array of {@link ClassRoom}s.
- */
+const r1 = new ClassRoom(19);
+const r2 = new ClassRoom(20);
+const r3 = new ClassRoom(34);
+
 export default function initializeRooms() {
-  return [19, 20, 34].map((size) => new ClassRoom(size));
+  return [
+    r1,
+    r2,
+    r3,
+  ];
 }
