349fc827b68be0314756953139369f7db5db18e4
leonileo <leolittleprogrammer@gmail.com>
2024-10-18 10:25:26 +0000

    hbtn

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
   // Getter
 
@@ -11,8 +11,8 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name !== 'string' ) {
-	throw new TypeError('Name must be a string');
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
@@ -28,7 +28,7 @@ export default class HolbertonCourse {
       throw new TypeError('Length must be a number');
     }
 
-    this._length = length;
+    this._length = value;
   }
 
   // Getter
